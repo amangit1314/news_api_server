9279b7ac1c66094e2842b79965083e6eb38d28bf
amangit1314 <gitaman8481@gmail.com>
2022-01-06 17:35:26 +0530

    node modules excluded

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const express = require('express');
-const render = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("We are on posts");
-});
-
-module.exports = router;
